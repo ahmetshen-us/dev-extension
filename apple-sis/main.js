51eb16a5e91894be8d07734ad78c7b564881375c
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-25 11:34:22 -0500

    made changes and added new functionality to apple-sis extension

--- apple-sis/main.js
+++ apple-sis/main.js
@@ -3,11 +3,22 @@ function myFunc(){
   try
   {
 document.getElementById("assignmentBar").style.width = "1000px";
-document.getElementById("gradeBar").style.width = "1000px";
+// document.getElementById("assignmentBar").style.paddingRight = "20px";
+// document.getElementById("assignmentBar").setAttribute("cellpadding","5px");
+var gradeBarElement = document.getElementById("gradeBar");
+gradeBarElement.style.width = "1000px";
+// gradeBarElement.style.paddingRight = "20px";
+
+// gradeBarElement.setAttribute("cellpadding","5px")
+
+
 document.getElementById("ScrollY").style.width = "1000px";
 var objDiv = document.getElementById("ScrollY");
 // objDiv.scrollTop = objDiv.scrollHeight;
 objDiv.scrollLeft = objDiv.scrollWidth;
+
+// var gradesTableElement = document.getElementById("gradesTable");
+// gradesTableElement.setAttribute("cellpadding","5px")
     
   }
 catch(err)
