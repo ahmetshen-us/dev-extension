51f24f71347c1183a6009f532ed3a5fa4c5b8063
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-24 21:00:42 -0500

    added an js extension for apple sis too

---
+++ apple-sis/main.js
@@ -0,0 +1,15 @@
+function myFunc(){
+
+  try
+  {
+document.getElementById("assignmentBar").style.width = "1000px";
+document.getElementById("gradeBar").style.width = "1000px";
+    
+  }
+catch(err)
+  {
+   
+  }
+
+}
+setTimeout(myFunc, 2000);
