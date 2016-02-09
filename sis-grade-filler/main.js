db1e256a2712c2319d27f14405753090e29980a8
ahmetshen-us <ahmetshen.us@gmail.com>
2016-02-09 14:43:29 -0500

    working and also got it working with ajax but will use .get()

--- sis-grade-filler/main.js
+++ sis-grade-filler/main.js
@@ -3,8 +3,11 @@ function myFunc(){
 
   try
   {
+// ===============================================
+/*
+var url = {"Dogukan Aciker": "https://www.codecademy.com/users/webJumper62951/achievements"};
 
-var url = "https://www.codecademy.com/users/textBlaster76324/achievements";
+
 var searchTerm ={unit5p1:"More on Control Flow in JS",
                 unit5p2:"Choose Your Own Adventure 2",
                 unit6p1:"Arrays and Objects in JS",
@@ -12,9 +15,13 @@ var searchTerm ={unit5p1:"More on Control Flow in JS",
 var point = 0;
 // var searchTerm ="Arrays and Objects in JS";
 $(function(){
-    $.get(url, function(data, status){
+  for(var st in url){
+    
+  }
+    $.get(url[st], function(data, status){
 //         alert("Data: " + data + "\nStatus: " + status);
 //       console.log(data.indexOf(searchTerm));
+     
       for (var key in searchTerm) {
 //   console.log("key " + key + " has value " + myArray[key]);
         if(data.indexOf(searchTerm[key])!=-1){
@@ -25,24 +32,25 @@ $(function(){
         }
 }
       console.log(point);
-    });
+    }
+         );
 });
+*/
+// ===============================================
 
+/*
+var response = '';
+$.ajax({ type: "GET",   
+         url:"https://www.codecademy.com/users/webJumper62951/achievements",   
+         async: false,
+         success : function(text)
+         {
+             response = text;
+         }
+});
+console.log(response.indexOf("More on Control Flow in JS")!=-1);
+*/
 
-
-
-
-
-
-
-    // leftArrowKey = 37
-    //    upArrowKey = 38
-    //    rightArrowKey = 39
-    //    downArrowKey = 40
-
-    //    e = jQuery.Event("keydown");
-    //    e.which = downArrowKey;
-    //    $("tGrd_r1_c24_cs0grd789903").trigger(e);
 
 
     
