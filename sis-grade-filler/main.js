a8f234c826852466c4c641a076fd905c69e61f73
ahmetshen-us <ahmetshen.us@gmail.com>
2016-02-09 15:48:00 -0500

    working version

--- sis-grade-filler/main.js
+++ sis-grade-filler/main.js
@@ -4,38 +4,57 @@ function myFunc(){
   try
   {
 // ===============================================
-/*
-var url = {"Dogukan Aciker": "https://www.codecademy.com/users/webJumper62951/achievements"};
+
+var nameUrl = {
+    "Dogukan Aciker": "https://www.codecademy.com/users/webJumper62951/achievements",
+  "Furkan Aldemir":"https://www.codecademy.com/users/furkan28aldemir/achievements"
+};
 
 
-var searchTerm ={unit5p1:"More on Control Flow in JS",
-                unit5p2:"Choose Your Own Adventure 2",
-                unit6p1:"Arrays and Objects in JS",
-                unit6p2:"Contact List"};
+var searchTerm = {
+    unit5p1: "More on Control Flow in JS",
+    unit5p2: "Choose Your Own Adventure 2",
+    unit6p1: "Arrays and Objects in JS",
+    unit6p2: "Contact List"
+};
 var point = 0;
-// var searchTerm ="Arrays and Objects in JS";
-$(function(){
-  for(var st in url){
-    
-  }
-    $.get(url[st], function(data, status){
-//         alert("Data: " + data + "\nStatus: " + status);
-//       console.log(data.indexOf(searchTerm));
-     
-      for (var key in searchTerm) {
-//   console.log("key " + key + " has value " + myArray[key]);
-        if(data.indexOf(searchTerm[key])!=-1){
-            console.log(key + " is finished");
-          point+=50;
-        }else {
-          point = 0;
-        }
-}
-      console.log(point);
+
+$(function() {
+    for (var name in nameUrl) {
+        callme(nameUrl[name], name);
     }
-         );
+
+    function callme(theUrl, newName) {
+        $.get(theUrl, function(data, status) {
+            // console.log("Data: " + data + "\nStatus: " + status);
+          
+            for (var key in searchTerm) {
+                //   console.log("key " + key + " has value " + myArray[key]);
+               
+
+                if (data.indexOf(searchTerm[key]) != -1) {
+//                     console.log(key + " is finished");
+                 
+          
+                   point += 50;
+                 
+                   
+              
+               }
+              
+          
+
+            }
+       
+            console.log(newName +" = "+point);
+             point=0;
+        });
+    }
+  console.clear();
 });
-*/
+
+
+
 // ===============================================
 
 /*
