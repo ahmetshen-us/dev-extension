179724fe08a1f20c8a7ded9224fd374a1b882c8e
ahmetshen-us <ahmetshen.us@gmail.com>
2016-02-09 13:09:16 -0500

    added jquery and sample example works with a link

--- sis-grade-filler/main.js
+++ sis-grade-filler/main.js
@@ -4,16 +4,41 @@ function myFunc(){
   try
   {
 
-// document.getElementById("assignmentBar").style.width = "1000px";
-// document.getElementById("assignmentBar").style.paddingRight = "50px";
-// // document.getElementById("assignmentBar").setAttribute("cellpadding","5px");
-// var gradeBarElement = document.getElementById("gradeBar");
-// gradeBarElement.style.width = "1000px";
-// gradeBarElement.style.paddingRight = "50px";
+var url = "https://www.codecademy.com/users/textBlaster76324/achievements";
+var searchTerm ={unit5p1:"More on Control Flow in JS",
+                unit5p2:"Choose Your Own Adventure 2"};
+var point = 0;
+// var searchTerm ="Arrays and Objects in JS";
+$(function(){
+    $.get(url, function(data, status){
+//         alert("Data: " + data + "\nStatus: " + status);
+//       console.log(data.indexOf(searchTerm));
+      for (var key in searchTerm) {
+//   console.log("key " + key + " has value " + myArray[key]);
+        if(data.indexOf(searchTerm[key])!=-1){
+            console.log(key + " is finished");
+          point+=50;
+        }else {
+          point = 0;
+        }
+}
+      console.log(point);
+    });
+});
 
-  if(window.location.href.indexOf("Grades") > -1) {
-       alert("your url contains the name franky");
-    }
+
+
+
+
+    // leftArrowKey = 37
+    //    upArrowKey = 38
+    //    rightArrowKey = 39
+    //    downArrowKey = 40
+
+    //    e = jQuery.Event("keydown");
+    //    e.which = downArrowKey;
+    //    $("tGrd_r1_c24_cs0grd789903").trigger(e);
+
 
     
   }
