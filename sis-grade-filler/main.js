89f30e8f9e2dbdbc61a331b92b9a2dfc4492b4d6
ahmetshen-us <ahmetshen.us@gmail.com>
2016-02-09 11:46:13 -0500

    search for text in current window

--- sis-grade-filler/main.js
+++ sis-grade-filler/main.js
@@ -4,39 +4,22 @@ function myFunc(){
   try
   {
 
-document.getElementById("assignmentBar").style.width = "1000px";
-document.getElementById("assignmentBar").style.paddingRight = "50px";
-// document.getElementById("assignmentBar").setAttribute("cellpadding","5px");
-var gradeBarElement = document.getElementById("gradeBar");
-gradeBarElement.style.width = "1000px";
-gradeBarElement.style.paddingRight = "50px";
+// document.getElementById("assignmentBar").style.width = "1000px";
+// document.getElementById("assignmentBar").style.paddingRight = "50px";
+// // document.getElementById("assignmentBar").setAttribute("cellpadding","5px");
+// var gradeBarElement = document.getElementById("gradeBar");
+// gradeBarElement.style.width = "1000px";
+// gradeBarElement.style.paddingRight = "50px";
 
-// gradeBarElement.setAttribute("cellpadding","5px")
+  if(window.location.href.indexOf("Grades") > -1) {
+       alert("your url contains the name franky");
+    }
 
-
-document.getElementById("ScrollY").style.width = "1000px";
-var objDiv = document.getElementById("ScrollY");
-// objDiv.scrollTop = objDiv.scrollHeight;
-objDiv.scrollLeft = objDiv.scrollWidth;
-
-
-
-// var gradesTableElement = document.getElementById("gradesTable");
-// gradesTableElement.setAttribute("cellpadding","5px")
     
   }
 catch(err)
   {
    
-  }
-
-  try{
-    document.getElementById("lStudents").style.height = "350px";
-        // console.log(document.getElementById("lStudents"));
-
-  } 
-  catch(err){
-
   }
 
 }
