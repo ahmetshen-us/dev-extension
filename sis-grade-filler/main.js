b5be493b510be4d63b45c23bbbf6416644011ac3
ahmetshen-us <ahmetshen.us@gmail.com>
2016-02-10 12:07:21 -0500

    P5 unit 7 graded

--- sis-grade-filler/main.js
+++ sis-grade-filler/main.js
@@ -5,26 +5,51 @@ function myFunc() {
         // ===============================================
 
 var nameUrl = {
-    "Dogukan Aciker": "https://www.codecademy.com/users/webJumper62951/achievements",
-  "Furkan Aldemir":"https://www.codecademy.com/users/furkan28aldemir/achievements",
-  "Ihsan Alkin":"https://www.codecademy.com/users/IhsanAlkin/achievements",
-  "Burak Bilgen":"https://www.codecademy.com/users/BurakBilgen/achievements",
-  "Yunus Celik":"https://www.codecademy.com/users/chipSolver75435/achievements",
-  "Abdulkerim Eygoren":"https://www.codecademy.com/users/systemPro77748/achievements",
-  "Omer Gece":"https://www.codecademy.com/users/omergece/achievements",
-  "Sajidur Kader":"https://www.codecademy.com/users/teraBlaster90154/achievements",
-  "Cankutay Katkat":"https://www.codecademy.com/users/arcAce82707/achievements",
-  "Selim Kopuz":"https://www.codecademy.com/users/SelimKopuz/achievements",
-  "Yavuz Korkmaz":"https://www.codecademy.com/users/courseJumper57960/achievements",
-  "Tashku Mishto":"https://www.codecademy.com/users/textRunner98433/achievements",
-  "Taha Nas":"https://www.codecademy.com/users/dataRockstar16388/achievements",
-  "Bugrahan Ozkaya":"https://www.codecademy.com/users/coreMaster38580/achievements",
-  "Ahmet Pamukcuoglu":"https://www.codecademy.com/users/javaSurfer71293/achievements",
-  "Dogukan Sahin":"https://www.codecademy.com/users/bitPro93681/achievements",
-  "Toprak Unsal":"https://www.codecademy.com/users/webRunner73460/achievements",
-  "Abdulkadir Talha Yilmaz":"https://www.codecademy.com/users/talhayilmaz/achievements",
-  "Isa Yolbarsov":"https://www.codecademy.com/users/Iyolbarsov/achievements"
-
+//   P4
+  //   ==============================================================================
+//     "Dogukan Aciker": "https://www.codecademy.com/users/webJumper62951/achievements",
+//   "Furkan Aldemir":"https://www.codecademy.com/users/furkan28aldemir/achievements",
+//   "Ihsan Alkin":"https://www.codecademy.com/users/IhsanAlkin/achievements",
+//   "Burak Bilgen":"https://www.codecademy.com/users/BurakBilgen/achievements",
+//   "Yunus Celik":"https://www.codecademy.com/users/chipSolver75435/achievements",
+//   "Abdulkerim Eygoren":"https://www.codecademy.com/users/systemPro77748/achievements",
+//   "Omer Gece":"https://www.codecademy.com/users/omergece/achievements",
+//   "Sajidur Kader":"https://www.codecademy.com/users/teraBlaster90154/achievements",
+//   "Cankutay Katkat":"https://www.codecademy.com/users/arcAce82707/achievements",
+//   "Selim Kopuz":"https://www.codecademy.com/users/SelimKopuz/achievements",
+//   "Yavuz Korkmaz":"https://www.codecademy.com/users/courseJumper57960/achievements",
+//   "Tashku Mishto":"https://www.codecademy.com/users/textRunner98433/achievements",
+//   "Taha Nas":"https://www.codecademy.com/users/dataRockstar16388/achievements",
+//   "Bugrahan Ozkaya":"https://www.codecademy.com/users/coreMaster38580/achievements",
+//   "Ahmet Pamukcuoglu":"https://www.codecademy.com/users/javaSurfer71293/achievements",
+//   "Dogukan Sahin":"https://www.codecademy.com/users/bitPro93681/achievements",
+//   "Toprak Unsal":"https://www.codecademy.com/users/webRunner73460/achievements",
+//   "Abdulkadir Talha Yilmaz":"https://www.codecademy.com/users/talhayilmaz/achievements",
+//   "Isa Yolbarsov":"https://www.codecademy.com/users/Iyolbarsov/achievements"
+//   ==============================================================================
+  //   P5
+  //   ==============================================================================
+  "Ahmet Akarsu":"https://www.codecademy.com/users/methodAce46548/achievements",
+  "Ahmet Aksu":"https://www.codecademy.com/users/microSurfer34186/achievements",
+  "Hamza Baris":"https://www.codecademy.com/users/byteMaster05043/achievements",
+  "Murat Berber":"https://www.codecademy.com/users/dataPro63482/achievements",
+  "Atakan Bilen":"https://www.codecademy.com/users/cssSolver47981/achievements",
+  "Asim Ciceksay":"https://www.codecademy.com/users/microSurfer10716/achievements",
+  "Kemal Dernek":"https://www.codecademy.com/users/textSlayer31184/achievements",
+  "Darwin Diaz":"https://www.codecademy.com/users/courseBlaster77354/achievements",
+  "Ertugrul Emiroglu":"https://www.codecademy.com/users/coreMaster88068/achievements",
+  "Furkan Goktepe":"https://www.codecademy.com/users/chipWhiz84045/achievements",
+  "Rafath Kader":"https://www.codecademy.com/users/teraJumper75054/achievements",
+  "Burhan Karabulut":"https://www.codecademy.com/users/cloudRockstar24840/achievements",
+  "Bilal Kemaloglu":"https://www.codecademy.com/users/chipSolver51649/achievements",
+  "Akhmet Mamedov":"https://www.codecademy.com/users/pyWhiz10376/achievements",
+  "Yousef Masri":"https://www.codecademy.com/users/microAce35449/achievements",
+  "Dean Motasis":"https://www.codecademy.com/users/textBlaster76324/achievements",
+  "Ilker Onal":"https://www.codecademy.com/users/teraRockstar81999/achievements",
+  "Guancheng Qiao":"https://www.codecademy.com/users/scriptMaster79852/achievements",
+  "Selim Yavuz":"https://www.codecademy.com/users/blogJumper35935/achievements",
+  "Enver Yeniyurt":"https://www.codecademy.com/users/tagCoder73722/achievements",
+  "Yusuf Celik":"https://www.codecademy.com/users/microSurfer10716/achievements"
 };
 
 var searchTerm = {
@@ -76,6 +101,8 @@ $(function() {
     }
   console.clear();
 });
+
+
 
 
 
