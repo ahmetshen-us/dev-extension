d3fbe7092eab216139fe18b76c9ac3b36453fc0b
ahmetshen-us <ahmetshen.us@gmail.com>
2016-03-15 12:34:52 -0400

    completed count uncompleted

--- khan-academy-check/main.js
+++ khan-academy-check/main.js
@@ -15,42 +15,81 @@ function myFunc() {
 
     // });
       var count = 0;
+      var completed = 0;
+      var uncompleted = 0;
     $('table span').each(function() {
       // console.log($(this).attr());
 
       // console.log($(this).hasClass("pipe")&&$(this).hasClass("completed"));
-      $this = $(this);
-      if($this.hasClass("pipe")&&$this.hasClass("completed")&&(!$this.hasClass("started"))){
-        count++;
-      }
+
       // if($(this).is( ".pipe.completed" )){
       //   count++;
       // }
 
+      $this = $(this);
+      if($this.hasClass("pipe")&&$this.hasClass("completed")&&(!$this.hasClass("started"))){
+        completed++;
+      }else if($this.hasClass("pipe")&&$this.hasClass("completed")&&($this.hasClass("started"))){
+        uncompleted++;
+      }
+
+      count =  completed+uncompleted;
+    // console.log(count);
+
+    // if(count == 10){
+    //   count =0;
+    //   completed =0 ;
+    //   uncompleted = 0;
+    //   alert("Part 1 Intro to HTML is Finished");
+
+    // }else if(count == 9){
+    //   count =0;
+    //   completed =0 ;
+    //   uncompleted = 0;
+    //   alert("Part 2 Intro to CSS is Finished");
+
+    // }
+    // else if(count == 8){
+    //   count =0;
+    //   completed =0 ;
+    //   uncompleted = 0;
+    //   alert("Part 3 More HTML tags is Finished");
+
+    // }
+    //  else if(count == 11){
+    //   count =0;
+    //   completed =0 ;
+    //   uncompleted = 0;
+    //   alert("Part 4 CSS Text properties is Finished");
+
+    // }
+    //  else if(count == 16){
+    //   count =0;
+    //   completed =0 ;
+    //   uncompleted = 0;
+    //   alert("Part 5 CSS Layout is Finished");
+
+    // }
+    //  else if(count == 11){
+    //   count =0;
+    //   completed =0 ;
+    //   uncompleted = 0;
+    //   alert("Part 6 More CSS Selectors is Finished");
+
+    // }
+    // else if(count == 6){
+    //   count =0;
+    //   completed =0 ;
+    //   uncompleted = 0;
+    //   alert("Part 7 Last Part")
+
+    // }
+
     });
+    // alert(count);
     alert(count);
-    if(count == 10){
-      alert("Part 1 Intro to HTML is Finished");
-    }else if(count == 19){
-      alert("Part 2 Intro to CSS is Finished");
+alert(completed);
 
-    }
-    else if(count == 27){
-      alert("Part 3 More HTML tags is Finished");
-
-    }
-     else if(count == 38){
-      alert("Part 4 CSS Text properties is Finished");
-
-    }
-     else if(count == 65){
-      alert("Part 6 More CSS Selectors is Finished");
-
-    }
-    else if(count == 71){
-      alert("Part 7 Last Part")
-
-    }
     // console.log(count);
 
 }
