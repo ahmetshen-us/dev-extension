89efb74e12011d571c0a514690201d346d97d78f
ahmetshen-us <ahmetshen.us@gmail.com>
2016-03-15 11:12:16 -0400

    target spans in each td in a table

--- khan-academy-check/main.js
+++ khan-academy-check/main.js
@@ -1,6 +1,17 @@
+function myFunc(argument) {
+
+      $('table td:nth-child('+2+')').each(function(index,value) {
+
+        var curValue = $(this).find("span");
+        console.log(curValue);
+      });
+
+}
+
 
 key('ctrl+q', function() {
 
     myFunc();
 
 });
+
